1ded7968884c982d13431ecba75265eae18d4b54
bhavani3334 <bhavani.23ise@cambridge.edu.in>
2026-06-24 18:31:04 +0530

    Movie app

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "a66213f8f27a1b45714aeded86ca8725";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
